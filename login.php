075eb73b8f8e9f56f10b6a148b025aaa16fd0f48
juan argoti <juanargoti2015@hotmail.com>
2019-05-03 19:46:36 -0500

    formulario para editar

---
+++ login.php
@@ -0,0 +1,48 @@
+<html>
+    <head>
+        <title>signup</title>
+    </head> 
+    <body bgcolor="#E0E0E0">
+        <litle><b>FORMULARIO REGISTRO</title>
+    </head>
+     <body>
+
+     <form>
+    <label for="nombre">Nombre:</label> <input type="txt" placeholder="Escribe tu mombre" maxlength="" name="nombre" id="nombre"><br> <imput type pattern=[A-Z] {3.15} title="solo se aceptan LETRAS">
+
+    <label for="nombre">password:</label>  <input type="txt" placeholder="password" maxlength="" name="nombre" id="nombre"><br>
+
+         
+         <center> <a href="login_up.php" target="BLANK"> </center><br>
+        
+
+    <center><input type="submit" value="REGISTRARME"></center><br>
+
+
+
+
+                            
+           
+
+              <form name="formulario" method="post" action="http://pagina.com/send.php">
+                <!-- Datos del formulario -->
+                
+                <!-- Botón de envío de formulario -->
+                <input type="submit" value="Enviar formulario">
+            </form>
+
+
+
+
+
+
+            
+
+
+
+       
+
+ </form>
+
+    </body>
+</html>
---
+++ login_up.php
@@ -0,0 +1,51 @@
+<html>
+    <head>
+        <title>signup</title>
+    </head> 
+    <body bgcolor="#E0E0E0">
+        <litle><b>FORMULARIO REGISTRO</title>
+    </head>
+     <body>
+
+     <form>
+    <label for="nombre">firstname:</label> <input type="txt" placeholder="Escribe tu mombre" maxlength="" name="nombre" id="nombre"><br> <imput type pattern=[A-Z] {3.15} title="solo se aceptan LETRAS">
+
+    <label for="nombre">lastname:</label>  <input type="txt" placeholder="password" maxlength="" name="nombre" id="nombre"><br>
+
+       <label for="nombre">email:</label> <input type="txt" placeholder="Escribe tu mombre" maxlength="" name="nombre" id="nombre"><br> <imput type pattern=[A-Z] {3.15} title="solo se aceptan LETRAS">
+
+    <label for="nombre">password:</label>  <input type="txt" placeholder="password" maxlength="" name="nombre" id="nombre"><br>
+  
+        
+        
+
+    
+
+
+
+
+                            
+           
+
+              <form name="formulario" method="post" action="http://pagina.com/send.php">
+                <!-- Datos del formulario -->
+                
+                <!-- Botón de envío de formulario -->
+                <input type="submit" value="Enviar formulario">
+            </form>
+
+
+
+
+
+
+            
+
+
+
+       
+
+ </form>
+
+    </body>
+</html>
